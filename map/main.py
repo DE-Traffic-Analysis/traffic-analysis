3a5b5e112d80607781c14b9fa9ed297c3b4f0550
김승훈 <eephus625@icloud.com>
2024-05-21 01:28:53 +0900

    refactor: 코드 리팩토링

--- map/main.py
+++ map/main.py
@@ -1,8 +1,9 @@
 import pandas as pd
-import time
 
 highway_df = pd.read_csv('highway.csv')
+highway_df.to_parquet("highway.parquet", compression=None)
 node_df = pd.read_csv('updated_node.csv', encoding='CP949')
+node_df.to_parquet("node.parquet", compression=None)
 
 columns = ['도로명', '구간', '이정']
 mileage_df = pd.DataFrame(columns=columns)
@@ -12,26 +13,26 @@ def find_cur_node_max_mileage(road_name):
     return highway_df[highway_df['도로명'] == road_name]['이정'].max()
 
 
-for index, _ in node_df.iterrows():
-    if index == len(node_df) - 1:
-        break
-
+for index in range(len(node_df) - 1):
     cur_node, nxt_node = node_df.loc[index], node_df.loc[index + 1]
     cur_node_mileage, nxt_node_mileage = round(cur_node['도로이정'], 1), round(nxt_node['도로이정'], 1)
 
-    # 고속도로 데이터에서의 최대 이정 값이 null이면 노드 데이터에는 있지만 고속도로 데이터에는 없는 도로이므로 continue
-    if pd.isnull(find_cur_node_max_mileage(cur_node['도로명'])):
+    cur_road_name = cur_node['도로명']
+    # 함수를 한 번 호출하여 변수에 담는 과정을 통해 여러 번 함수를 호출하지 않음
+    cur_max_mileage = find_cur_node_max_mileage(cur_road_name)
+
+    if pd.isnull(cur_max_mileage):
         continue
     
-    if cur_node['도로명'] != nxt_node['도로명']:
-        # 만약 현재 행의 도로이정 값이 고속도로 데이터에서도 최대값이라면 continue
-        if cur_node_mileage >= find_cur_node_max_mileage(cur_node['도로명']):
+    if cur_road_name != nxt_node['도로명']:
+        if cur_node_mileage >= cur_max_mileage:
             continue
         else:
-            # 최대값이 아니라면 최대 값을 가져와서 도로의 끝을 표시
-            mileage_df = pd.concat([mileage_df, pd.DataFrame({'도로명': [cur_node['도로명']],
-                                            '구간': [cur_node['노드명'] + "-" + "도로의 끝"],
-                                            '이정': [[cur_node_mileage, find_cur_node_max_mileage(cur_node['도로명'])]]})], ignore_index=True)
+            mileage_df = pd.concat([mileage_df, pd.DataFrame({
+                '도로명': [cur_road_name],
+                '구간': [cur_node['노드명'] + "-" + "도로의 끝"],
+                '이정': [[cur_node_mileage, cur_max_mileage]]
+            })], ignore_index=True)
     else:
         # 가져온 csv 파일에 같은 노드명이 연속 두 번 나오는 경우가 있어서 예외
         if cur_node['노드명'] == nxt_node['노드명']:
@@ -42,24 +43,31 @@ for index, _ in node_df.iterrows():
         # 만약 현재 노드의 이정이 다음 노드의 이정보다 크다면 예외
         if cur_node_mileage > nxt_node_mileage:
             continue
-        mileage_df = pd.concat([mileage_df, pd.DataFrame({'도로명': [cur_node['도로명']],
-                                        '구간': [cur_node['노드명'] + "-" + nxt_node['노드명']],
-                                        '이정': [[cur_node_mileage, nxt_node_mileage]]})], ignore_index=True)
+        mileage_df = pd.concat([mileage_df, pd.DataFrame({
+            '도로명': [cur_road_name],
+            '구간': [cur_node['노드명'] + "-" + nxt_node['노드명']],
+            '이정': [[cur_node_mileage, nxt_node_mileage]]
+        })], ignore_index=True)
 
-mileage_df.to_csv('mileage_info.csv', sep=';', index=False)
+# 팀원에게 도움이 되기 위한 코드
+# mileage_df.to_csv('mileage_info.csv', sep=';', index=False)
 
 columns = ['name', 'section', 'path_json']
 df = pd.DataFrame(columns=columns)
 
-for index, _ in mileage_df.iterrows():
+# 그룹화된 DataFrame을 미리 만들어서 반복문에서 사용
+# 그룹화된 DataFrame은 데이터를 미리 인덱싱하여 빠른 조회가 가능하고, 반복적인 필터링을 제거하여 성능을 향상시킨다.
+highway_grouped = highway_df.groupby(['도로명', '이정'])
+
+for index in range(len(mileage_df)):
     data = mileage_df.loc[index]
     road_name, section, mileage = data['도로명'], data['구간'], data['이정']
     coordinates = []
 
     for m in range(int(mileage[0] * 10), int(mileage[1] * 10 + 1)):
-        m /= 10
-        selected_rows = highway_df[(highway_df['도로명'] == road_name) & (highway_df['이정'] == m)]
-        if not selected_rows.empty:
+        key = (road_name, round(m, 1))
+        if key in highway_grouped.groups:
+            selected_rows = highway_grouped.get_group(key)
             x = selected_rows['X좌표값'].iloc[0]
             y = selected_rows['Y좌표값'].iloc[0]
 
@@ -69,9 +77,10 @@ for index, _ in mileage_df.iterrows():
             else:
                 coordinates.append([y, x])
     if coordinates:
-        df = pd.concat([df, pd.DataFrame({'name': [road_name],
-                                        'section': [road_name + "(" + section + ")"],
-                                        'path_json': [coordinates]})], ignore_index=True)
+        df = pd.concat([df, pd.DataFrame({
+            'name': [road_name],
+            'section': [road_name + "(" + section + ")"],
+            'path_json': [coordinates]
+        })], ignore_index=True)
 
 df.to_csv('map.csv', sep=';', index=False)
-        
